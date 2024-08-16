7e8116319ff025404b199a9caf8ad57572a875e3
SebastianBartuzi <137120176+SebastianBartuzi@users.noreply.github.com>
2024-08-17 00:33:26 +0200

    Authorisation, update install

--- install.py
+++ install.py
@@ -4,7 +4,7 @@ import subprocess
 def install_packages():
     try:
         # Run the pip install command
-        subprocess.check_call(['pip', 'install', 'docx2pdf', 'python-docx', 'openpyxl'])
+        subprocess.check_call(['pip', 'install', 'docx2pdf', 'python-docx', 'openpyxl', 'pywin32'])
         print("Packages installed successfully!")
     except subprocess.CalledProcessError as e:
         print("An error occurred while installing packages:", e)
